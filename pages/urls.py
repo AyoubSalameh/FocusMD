e23719f462cbe0e0d4fa10917f4e16d3a391ec52
ayoubsalameh <salamehayoub@gmail.com>
2023-03-22 23:35:13 +0200

    yo

--- pages/urls.py
+++ pages/urls.py
@@ -4,4 +4,4 @@ from . import views
 urlpatterns = [
     path('', views.home_view, ),
     path('summary/', views.summary_view, ),
-]
+    path('patient/', views.patient_view, ),]
--- pages/views.py
+++ pages/views.py
@@ -1,9 +1,19 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+def foo(text):
+    return text #LLM
+
 # Create your views here.
 def home_view(request):
     return render(request, 'home.html', {})
 
+def patient_view(request):
+    return render(request, 'patient.html', {})
+
 def summary_view(request):
-    return render(request, 'summary.html', {})
+    output = None
+    if request.method == 'POST':
+        text = request.POST.get('text')
+        output = foo(text)
+    return render(request, 'summary.html', {'output': output})
